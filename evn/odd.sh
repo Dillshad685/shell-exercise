48e85559ff1c0975f9761f9aa9ec3da6d41df064
Dillshad685 <dillshadb2@gmail.com>
2026-01-26 23:49:38 +0530

    first commit

---
+++ evn/odd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Number=$1
+
+echo "entered number is : $Number"
+
+if [ $Number %2 -eq 0 ];
+    echo "Entered number $Number is even"
+else
+    echo "Entered number $Number is odd"
+fi
+
