435071e17e28cb1b89e97105dda5b0a73acda1c7
Dillshad685 <dillshadb2@gmail.com>
2026-01-27 19:39:45 +0530

    commit

---
+++ even-odd.sh
@@ -0,0 +1,14 @@
+#program to write whether even or odd?
+
+#!/bin/bash
+
+num=$1
+
+echo "enter the number $num"
+
+if [ $($num % 2) -eq 0 ]; then
+    echo "entered number $num is even"
+else
+    echo "entered number $num is odd"
+fi
+
