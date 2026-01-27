600bd9680201bf8c815b1bf9605d577058742d49
Dillshad685 <dillshadb2@gmail.com>
2026-01-27 19:41:34 +0530

    commit

--- even-odd.sh
+++ even-odd.sh
@@ -6,7 +6,7 @@ num=$1
 
 echo "enter the number $num"
 
-if [ $($num % 2) -eq 0 ]; then
+if [ $(($num %2)) -eq 0 ]; then
     echo "entered number $num is even"
 else
     echo "entered number $num is odd"
